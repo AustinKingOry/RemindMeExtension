eb10247ce41b75f8be3bdeb4db83504dca8125d2
The White Hat KE <austinkings30@gmail.com>
2025-02-13 17:55:09 +0300

    configuration for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+ /** @type {import('tailwindcss').Config} */
+ module.exports = {
+  content: ["./popup.html", "./settings.html", "./background.js", "./popup.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
